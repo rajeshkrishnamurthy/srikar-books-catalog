1fe94653c681acc0dba5063b370cfa956ddb6759
myla412 <rajesh.k@sumerusolutions.com>
2025-10-27 20:27:16 +0530

    Sync config.prod.js and config.staging.js from main

---
+++ scripts/config.prod.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAuhlwiSczUN5z3hAkp-hBrLPP4q-tpz_Y',
+  authDomain: 'srikar-book-deals.firebaseapp.com',
+  projectId: 'srikar-book-deals',
+  storageBucket: 'srikar-book-deals.firebasestorage.app',
+  messagingSenderId: '129722793187',
+  appId: '1:129722793187:web:fd932eb62f4626c79f59d6',
+};
+export const settings = {
+  whatsappNumber: '918951395971', // no + or spaces
+};
---
+++ scripts/config.staging.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDMq3eJpbQyFW3nzxYlHccCECfwNVlU7vg',
+  authDomain: 'srikar-book-deals-staging.firebaseapp.com',
+  projectId: 'srikar-book-deals-staging',
+  storageBucket: 'srikar-book-deals-staging.firebasestorage.app',
+  messagingSenderId: '457466282027',
+  appId: '1:457466282027:web:b44f89d43fd47f52cfea6e',
+};
+export const settings = {
+  whatsappNumber: '918951395971', // no + or spaces
+};
